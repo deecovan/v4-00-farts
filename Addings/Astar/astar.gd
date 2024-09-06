c7918c5f1b311570b51e677a9e7ed246904643c0
cheva <dmitry.cheva@gmail.com>
2024-09-07 01:28:47 +0300

    Tested working astar_array get_static_astar(target)

--- Addings/Astar/astar.gd
+++ Addings/Astar/astar.gd
@@ -4,43 +4,20 @@ extends Node2D
 # Astar grid
 @onready var astar_grid = AStarGrid2D.new()
 var static_tilemap_layer: TileMapLayer
-var tilemap_path: Array
 var static_tile_size: Vector2i
 
 
-func _ready() -> void:
-	pass
-
-
-func _process(_delta: float) -> void:
-	pass
-
-
 func get_astar(from_position: Vector2, to_position: Vector2) -> Array:
-	tilemap_path = astar_grid.get_point_path(
+	var tp := astar_grid.get_point_path(
 		pos_to_point(from_position), pos_to_point(to_position)
 		)
-	return tilemap_path
-	
-	
-func pos_to_point(pos: Vector2) -> Vector2i:
-	var point: Vector2i
-	pos -= self.global_position
-	point.x = int(pos.x / static_tile_size.x) 
-	point.y = int(pos.y / static_tile_size.y)
-	print("pos_to_point: ", pos, point)
-	return point
-	
-	
-func point_to_pos(point: Vector2) -> Vector2i:
-	var pos: Vector2i
-	var half_tile := static_tile_size / 2
-	pos.x = int(point.x) * static_tile_size.x + half_tile.x
-	pos.y = int(point.y) * static_tile_size.y + half_tile.y
-	print("point_to_pos: ", pos, point)
-	return pos
-	
-	
+	var half_tile := Vector2(static_tile_size / 2)
+	var fix_tp_arr: Array[Vector2]
+	for fix_tp in tp:
+		fix_tp_arr.append(fix_tp - half_tile)
+	return fix_tp_arr
+
+
 func set_astar() -> void:
 	# Set up parameters, then update the grid.
 	astar_grid.region = static_tilemap_layer.get_used_rect()
@@ -50,10 +27,7 @@ func set_astar() -> void:
 	# NOT All used cells are obstacles
 	for tile in get_used_static_cells():
 		astar_grid.set_point_solid(tile,true)
-	# Test Astar
-	tilemap_path = astar_grid.get_point_path(Vector2i(0,0), Vector2i(2, 0))
-	print(tilemap_path)
-	
+
 
 func get_used_static_cells() -> Array[Vector2i]:
 	var coords_array: Array[Vector2i]
@@ -66,11 +40,26 @@ func get_used_static_cells() -> Array[Vector2i]:
 				coords_array.append(Vector2i(x, y))
 	return coords_array
 
-	
+
 func get_apath(from_position, to_position) -> PackedVector2Array:
 	var from = static_tilemap_layer.local_to_map(from_position)
 	var to = static_tilemap_layer.local_to_map(to_position)
-	tilemap_path = astar_grid.get_point_path(from, to)
-	return tilemap_path
+	return astar_grid.get_point_path(from, to)
 
-	
+
+func pos_to_point(pos: Vector2) -> Vector2i:
+	var point: Vector2i
+	pos -= self.global_position
+	point.x = int(pos.x / static_tile_size.x) 
+	point.y = int(pos.y / static_tile_size.y)
+	print("pos_to_point: ", pos, point)
+	return point
+
+## Not used ???
+#func point_to_pos(point: Vector2) -> Vector2i:
+	#var pos: Vector2i
+	#var half_tile := static_tile_size / 2
+	#pos.x = int(point.x) * static_tile_size.x + half_tile.x
+	#pos.y = int(point.y) * static_tile_size.y + half_tile.y
+	#print("point_to_pos: ", pos, point)
+	#return pos
