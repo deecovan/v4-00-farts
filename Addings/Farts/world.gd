4e5d94f5032b748166b0db277ce667ce6306600f
cheva <dmitry.cheva@gmail.com>
2024-09-08 01:11:50 +0300

    Start init NPC's

--- Addings/Farts/world.gd
+++ Addings/Farts/world.gd
@@ -6,37 +6,40 @@ extends Node2D
 @onready var camera: Camera2D = $Camera2D
 
 const base_npc_tscn := "res://Addings/Farts/chars/base_npc.tscn"
+const base_npc_gd := "res://Addings/Farts/chars/base_npc.gd"
 
 func _ready() -> void:
 	## Instantiate NPC's
 	ResourceLoader.load_threaded_request(base_npc_tscn)
 	inst_npcs()
-	## Than init all in group
-	init_npcs()
 	
 	astar.global_center = get_viewport().get_visible_rect().get_center()
 	astar.static_tilemap_layer = statics
 	astar.static_tile_size = statics.tile_set.tile_size
 	astar.set_astar()
 
+	## Init all in group NPC's
+	init_npcs()
 
 func inst_npcs() -> void:
 	var new_npc_res := ResourceLoader.load_threaded_get(base_npc_tscn)
 	var new_npc = new_npc_res.instantiate()
-	new_npc.position = Vector2(60,60)
-	add_child(new_npc, true)
+	#new_npc.position = Vector2(-1000, -1000)
+	#new_npc.add_to_group("NPC")
+	#new_npc.set_script(base_npc_gd)
+	#add_child(new_npc, true)
+	#var ccc = CharacterBody2D.new()
 
 
 func init_npcs() -> void:
 	for child in get_children():
 		if child.is_in_group("NPC"):
-			var base_npc: CharacterBody2D = $BaseNPC
-			print(base_npc.get_instance_id())
-			base_npc.tile_size = statics.tile_set.tile_size
-			base_npc.camera = camera
-			base_npc.func_get_astar_path = astar.get_astar_path
-			base_npc.func_get_free_static_cells = astar.get_free_static_cells
-			base_npc.global_center = get_viewport().get_visible_rect().get_center()
+			print(child.get_instance_id())
+			child.tile_size = statics.tile_set.tile_size
+			child.camera = camera
+			child.func_get_astar_path = astar.get_astar_path
+			child.func_get_free_static_cells = astar.get_free_static_cells
+			child.global_center = get_viewport().get_visible_rect().get_center()
 
 
 func _process(_delta: float) -> void:
