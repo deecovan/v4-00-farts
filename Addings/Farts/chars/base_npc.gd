f1cb888103bc071f4626681676fcad12bb3e245f
cheva <dmitry.cheva@gmail.com>
2024-09-08 13:03:03 +0300

    Describe FSM in comments

--- Addings/Farts/chars/base_npc.gd
+++ Addings/Farts/chars/base_npc.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+
 @export var dexta := 5.0
 @export var speed := 200.0
 @export var shift := 400.0
@@ -14,6 +15,7 @@ var timer: float = 0.0
 var global_center : Vector2
 var target : Vector2
 
+
 func _init() -> void:
 	$Cross.default_color = Color(
 		randf_range(0.75,1),
@@ -21,10 +23,12 @@ func _init() -> void:
 		randf_range(0.25,0.75),)
 	print(name, " ready")
 
+
 func _physics_process(delta: float) -> void:
 	## Lerp physics
 	velocity = lerp(velocity, get_input(delta) * speed, delta * dexta)
 	move_and_slide()
+	
 	
 func get_input(delta) -> Vector2:
 	timer += delta
@@ -44,16 +48,42 @@ func get_input(delta) -> Vector2:
 		#velocity *= -normal * Vector2(
 			#randf_range(-1,1), randf_range(-1,1))
 		velocity = Vector2.ZERO
-			
-	## If state == stuck
-	## If state == iddle
+
+
+	## If State == * and near is another Speaking
+	## --> State Ignore -> State Moving
+	## --> State Stop -> State Hearing -> State Decide
+	
+	## If State == Decide 
+	## --> Speaking
+	## --> Confirm
+	## --> Deny	
+	
+	## If State == Folowing and target NPC is near
+	## -> State Speaking
+	
+	## If State == Moving and near is target Position
+	## -> State Idle
+	
+	## If State == Moving and Target Position not changing
+	## -> State Stuck
+	
+	## If State == Stuck
+	## If State == Iddle
+	## -> State Looking 
+	## --> State Pick new target (NPC or position) 
+	## ---> State Moving to target
+	
+	## Stuck/Idle -> Moving
 	if velocity.length() < 100 and timer > 3:
 		timer = 0.0
 		while not set_current_target(get_random_position()):
 			print("%s can't move to %s, re-target" % [name, target])
-		print(name, " mooving to ", target)
+		
+		print(name, " Moving to ", target)
 		
 	return velocity_to
+
 
 func get_random_position() -> Vector2:
 	var free_cells: Array[Vector2] = func_get_free_static_cells.call()
@@ -78,8 +108,8 @@ func get_current_star() -> Vector2:
 	if (star - self.global_position).length() < Vector2(tile_size / 2).length():
 		astar_array.remove_at(0)
 	return star
-	
-	
+
+
 func set_current_target(targeted: Vector2) -> bool:
 	target = targeted as Vector2
 	astar_array = get_static_astar(target)
--- Addings/Farts/world.gd
+++ Addings/Farts/world.gd
@@ -22,9 +22,10 @@ func _ready() -> void:
 func inst_npcs(q:int) -> void:
 	for i in q:
 		var new_npc = base_npc_tscn.instantiate()
+		## Randomise spawn initial position
 		new_npc.position = Vector2(
-			-1000 - randi_range(100,1000), 
-			-1000 - randi_range(100,1000))
+			-1000 - randi_range(100,100), 
+			-1000 - randi_range(100,100))
 		add_child(new_npc, true)
 		new_npc.add_to_group("NPC")
 		new_npc.set_script(base_npc_gd)
@@ -33,12 +34,14 @@ func inst_npcs(q:int) -> void:
 
 func init_npcs() -> void:
 	for child in get_children():
+		## Check is it new NPC
 		if child.is_in_group("NPC") and child.timer == 0.0:
 			child.tile_size = statics.tile_set.tile_size
 			child.camera = camera
 			child.func_get_astar_path = astar.get_astar_path
 			child.func_get_free_static_cells = astar.get_free_static_cells
 			child.global_center = get_viewport().get_visible_rect().get_center()
+			## Randomise position and target
 			child.set_random_position()
 			child.set_current_target(child.get_random_position())
 			print(child.get_instance_id(), child.global_position, child.target)
