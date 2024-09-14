dfee9a5fea556e57408d3a15bad8a6fffdaf10e1
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-09-14 18:05:42 +0300

    Continue Diffuse Abe

--- Addings/Farts/chars/Abes/diffuse.gd
+++ Addings/Farts/chars/Abes/diffuse.gd
@@ -15,15 +15,18 @@ extends Node2D
 
 func _ready() -> void:
 	amount = 10
+	square = $Square
+	from = $from
+	to = $to
 
 
 func _process(delta: float) -> void:
-	if(amount > 0):
+	if(amount > 0 and from != null and to != null):
+		print(from)
 		square.show()
 		var coords: Vector2
 		coords = lerp(
 			from.global_position, to.global_position, timer * amount)
-		print(coords)
 		square.global_position = coords
 		timer += delta
 		amount -= 1
