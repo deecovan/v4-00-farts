5924eda3111b77c1cc40a3087dbbd803b980e9a0
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-09-14 14:09:56 +0300

    Draw Square

--- Addings/Farts/chars/test.gd
+++ Addings/Farts/chars/test.gd
@@ -1,20 +1,30 @@
 extends Node2D
 
+@export var texture : Texture2D:
+	set(value):
+		texture = value
+		queue_redraw()
+		
+@export var polygon: Polygon2D:
+	set(value):
+		polygon = value
+		queue_redraw()
 
-func _draw() -> void:
+var sqarray  :  Array[Vector2]
+var square   :  PackedVector2Array
+var color    :  Color
 
-	var from_pos = randi_range(-200,200)
-	var to_pos = randi_range(-200,200)
-	var amount = 1
-	
-	for i in amount:
-		var sqarray := Array([[0,0],[0,64],[64,64],[64,0]])
-		var square = PackedVector2Array(sqarray)
-		var polygon: Polygon2D = Polygon2D.new()
-		var color = Color.CORAL
-		polygon.draw_colored_polygon(sqarray, color)
-		add_child(polygon)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _draw():
+	## Test Draw Texture
+	draw_texture(texture, Vector2())
+	## Draw Square
+	sqarray = [
+		Vector2(0,0),
+		Vector2(0,64),
+		Vector2(64,64),
+		Vector2(64,0),
+		]
+	square = PackedVector2Array(sqarray)
+	color = Color.CORAL
+	draw_colored_polygon(square, color)
